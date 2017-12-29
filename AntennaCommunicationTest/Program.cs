741cbe5313c0c4623dda4244771fd641081d68f2
reddraptor <sdmuir76@gmail.com>
2017-12-29 12:29:32 -0800

    Hercules H can now communicate with station via antenna to open and close bay doors. Station Operations Module has own script now to redirect transmissions to other module scripts.

---
+++ AntennaCommunicationTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        string antennaName = "Antenna";
+
+        IMyRadioAntenna antenna;
+
+        public Program()
+        {
+            antenna = (IMyRadioAntenna)GridTerminalSystem.GetBlockWithName(antennaName);
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Main(string argument, UpdateType source)
+        {
+            if ((source & UpdateType.Terminal) != 0)
+            {
+                string[] words = argument.Split(' ');
+
+                if (words[0] == "transmit")
+                {
+                    antenna.TransmitMessage(argument.Substring("transmit ".Length));
+                }
+            }
+        }
+    }
+}
